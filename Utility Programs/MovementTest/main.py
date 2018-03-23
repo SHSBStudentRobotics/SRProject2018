399e5a673353f52256aecba69ca22a4e8cec8fab
Joshua Smith <joshuajames355@gmail.com>
2018-03-23 13:34:16 +0000

    Updated movement test for the new config system.

--- Utility Programs/MovementTest/main.py
+++ Utility Programs/MovementTest/main.py
@@ -4,15 +4,18 @@ import time
 
 robot = Robot()
 
+config = {}
+config["Hardware"] = {"CurrentProtectionMaxChange" : "0.5"}
+
 while True:
-    for x in range(10):
-        powerMotor(robot, 0, 1)
-        powerMotor(robot, 1, 1)
+    for x in range(50):
+        powerMotor(robot, 0, 1, config)
+        powerMotor(robot, 1, 1, config)
 
         time.sleep(0.5)
 
     for x in range(10):
-        powerMotor(robot, 0, -1)
-        powerMotor(robot, 1, -1)
+        powerMotor(robot, 0, -1, config)
+        powerMotor(robot, 1, -1, config)
 
         time.sleep(0.5)
